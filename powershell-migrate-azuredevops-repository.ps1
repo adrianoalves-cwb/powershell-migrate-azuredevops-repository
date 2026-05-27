095d788993050ddf9a1c26ebdcf58283af878dfc
Adriano Alves da Silva <58754259+adrianoalves-cwb@users.noreply.github.com>
2026-05-27 08:06:24 -0300

    Adding a fix to the script

--- powershell-migrate-azuredevops-repository.ps1
+++ powershell-migrate-azuredevops-repository.ps1
@@ -1,27 +1,84 @@
+
 Param
 (
-    [Parameter(Mandatory = $true, Position = 0)]
-    [string] $Origin,
-
-    [Parameter(Mandatory = $true, Position = 1)]
-    [string] $Destination
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $origin,
+        [Parameter(Mandatory=$true, Position=1)]
+        [string] $destination
 )
 
-$TempFolderPath = "C:\temp"
+$newFolder  = "C:\temp"
 
-$date = Get-Date -Format "yyyyMMdd_HHmmss"
-$newFolder = Join-Path $TempFolderPath $date
-
-if (Test-Path $newFolder) {
-    Remove-Item $newFolder -Recurse -Force
+Function RemoveFolderIfExists($folderPath )
+{
+    If (Test-Path -Path $folderPath)
+    {
+        Write-Host "The folder $folderPath exists. Removing it..."
+        Remove-Item $folderPath -Force
+    }
 }
 
-New-Item -Path $newFolder -ItemType Directory | Out-Null
+Function CreateNewFolder($folderPath )
+{
+    if (!(Test-Path $folderPath))
+    {
+        Write-Host "Creating the new folder: $folderPath"
+        New-Item -ItemType Directory -Path $folderPath | Out-Null
+    }
+}
+
+Function CheckoutBranch($branch)
+{
+    Write-Host "Checking if the Git branch $branch exists..."
+
+    $Result = git checkout $branch
+
+    if ($Result -match 'error')
+    {
+        Write-Host "branch $branch was NOT Found!"
+
+        return $false
+    }
+
+    return $true
+}
+
+$date = get-date -Format "yyyyMMdd_HHmmsss"
+
+$newFolder = $newFolder  + "\"+ $date
+
+RemoveFolderIfExists($newFolder)
+CreateNewFolder($newFolder)
+
 Set-Location $newFolder
 
-git -c http.sslVerify=false clone --mirror $Origin
-$repoFolder = Get-ChildItem -Path $newFolder -Directory | Select-Object -First 1
-Set-Location $repoFolder.FullName
+$env:GIT_REDIRECT_STDERR = '2>&1'
 
-git remote set-url --push origin $Destination
-git push --mirror
+Invoke-Expression "git -c http.sslVerify=false clone $origin"
+
+$subFolder = Get-ChildItem -Directory $newFolder
+
+Set-Location $subFolder
+
+Write-Host "Getting the list of Git branches from the origin Repository..."
+$GitBranches = git for-each-ref --format='%(refname:short)' refs/remotes/origin `
+| Where-Object { $_ -ne 'origin/HEAD' } `
+| ForEach-Object { $_ -replace '^origin/', '' }
+
+foreach ($branch in $gitbranches)
+{
+    if ($branch -eq "origin"){
+        continue
+    }
+
+    Write-Host "Cloning Branch: " $branch
+    
+    if (CheckoutBranch($branch) -eq true)
+    {
+        Invoke-Expression "git config --global http.sslVerify false"
+        Invoke-Expression "git clone --mirror $destination"
+        Invoke-Expression "git push --mirror $destination"
+    }
+}
+
+Set-Location $PSScriptRoot
